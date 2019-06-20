458e2ca413818280a7c74cdd3f7349b3c7d3209c
Hilderlan <hilakt9698@gmail.com>
2019-06-20 17:52:07 -0300

    Criado a subclasse Artigo e seus metodos de acesso.

---
+++ Artigo.java
@@ -0,0 +1,9 @@
+public class Artigo extends Exemplar{
+    private String revista;
+
+    public String getRevista(){ return revista; }
+
+    public void setRevista(String revista){
+        this.revista = revista;
+    }
+}
