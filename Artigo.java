34c5f3c21fc8b4569e34ea268bcb75e11a7140ea
Hilderlan <hilakt9698@gmail.com>
2019-06-21 13:49:53 -0300

    Modificacoes

--- Artigo.java
+++
@@ -1,9 +0,0 @@
-public class Artigo extends Exemplar{
-    private String revista;
-
-    public String getRevista(){ return revista; }
-
-    public void setRevista(String revista){
-        this.revista = revista;
-    }
-}
--- Soma.java
+++
@@ -1,6 +0,0 @@
-public class Soma {
- 
-    public Integer sum(Integer a, Integer b){
-       return a + b;
-    }    
-}
