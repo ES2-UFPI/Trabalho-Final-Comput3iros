15860b7aa477be59bb785c957bc2b35191c80756
mtb <mateustitob@hotmail.com>
2019-06-21 16:22:12 -0300

    Cadastro de Locatario com verificaçao

---
+++ src/Locadora.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class Locadora {
+    private ArrayList<Locatario> locatarios = new ArrayList<Locatario>();
+
+    public void cadastrarLocatario(Locatario l) {
+        if (l.validaLocatario(l) == true) {
+            locatarios.add(l);
+            System.out.println("\nLocatario cadastrado com sucesso.");
+        } else {
+            System.out.println("\nNão foi possivel cadastrar locatario.");
+        }
+    }
+}
--- src/Locatario.java
+++ src/Locatario.java
@@ -3,22 +3,53 @@ public class Locatario {
     private String nome;
     private String categoria;
     private String senha;
-    
-    public String getMatricula() { return matricula; }
-    public String getNome() { return nome; }
-    public String getCategoria() { return categoria; }
-    public String getSenha() { return senha; }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
 
     public void setMatricula(String matricula) {
         this.matricula = matricula;
     }
+
     public void setNome(String nome) {
         this.nome = nome;
     }
+
     public void setCategoria(String categoria) {
         this.categoria = categoria;
     }
+
     public void setSenha(String senha) {
         this.senha = senha;
+    }
+
+    // varificação dos dados para cadastro de locatario
+    public boolean validaLocatario(Locatario l) {
+        if ((l.getMatricula().length() < 4) || (l.getSenha().length() < 4) || (l.getCategoria().length() == 0)
+                || (l.getCategoria().length() < 4)) {
+
+            // evitar cadastro com matricula igual
+            for (int i = 0; i < locatarios.size(); i++) {
+                if (locatarios.get(i).getMatricula().equals(l.getMatricula())) {
+                    return false;
+                }
+            }
+            return false;
+        } else {
+            return true;
+        }
     }
 }
