c0ae1c93c7fe4258073cbc80af1530245023412a
mtb <mateustitob@hotmail.com>
2019-06-22 20:59:53 -0300

    Removido os class

--- src/MenuPrincipal.java
+++ src/MenuPrincipal.java
@@ -1,12 +1,11 @@
 import java.util.Scanner;
 
 public class MenuPrincipal {
-
-    // menu principal
+    // Menu principal
     public void menu() {
 
-        System.out.println("\n\n### Locadora - Sistema de Empréstimos e Devoluções ###");
-        System.out.println("\n                ===================================");
+        System.out.println("\n        ### Locadora - Sistema de Empréstimos e Devoluções ###");
+        System.out.println("\n                  ===================================");
         System.out.println("                  |     1 - Cadastro                |");
         System.out.println("                  |     2 - Empréstimo              |");
         System.out.println("                  |     3 - Devolução               |");
@@ -17,29 +16,30 @@ public class MenuPrincipal {
         System.out.println("                  ===================================\n");
 
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Opção ->  ");
+        System.out.print("                  Opção ->  ");
         int opcao = scanner.nextInt();
 
         while (opcao < 0 || opcao > 6) {
-            System.out.print("Opção ->  ");
+            System.out.print("                  Opção ->  ");
             opcao = scanner.nextInt();
             scanner.close();
         }
 
         switch (opcao) {
-        case 1:
+        case 1: // Chamar submenu Cadastro
+            SubMenuCadastro.menuCadastro();
             break;
-        case 2:
+        case 2: // Cadastrar emprestimo
             break;
-        case 3:
+        case 3: // Cadastrar devolucao
             break;
-        case 4:
+        case 4: // Cadastrar relatorio
             break;
-        case 5:
+        case 5: // Pesquisar
             break;
-        case 6:
+        case 6: // Alterar Configuracao
             break;
-        case 0:
+        case 0: // Sair
             break;
         default:
             System.out.println("Opção Inválida!");
