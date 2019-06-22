cdcd6dd319a6f6d183b08d46f5a46d0cb01aafef
mtb <mateustitob@hotmail.com>
2019-06-22 20:54:38 -0300

    Bugs emprestimo fixed

--- src/Emprestimo.java
+++ src/Emprestimo.java
@@ -1,3 +1,5 @@
+import java.util.Date;
+
 public class Emprestimo {
     private Exemplar exemplar;
     private Locatario locatario;
@@ -5,32 +7,42 @@ public class Emprestimo {
     private Date dataDevol;
     private boolean isDevolvido;
 
-    public Locatario getLocatario() { return locatario; }
+    public Locatario getLocatario() {
+        return locatario;
+    }
 
     public void setLocatario(Locatario locatario) {
         this.locatario = locatario;
     }
+
     public Exemplar getExemplar() {
         return exemplar;
     }
+
     public void setExemplar(Exemplar exemplar) {
         this.exemplar = exemplar;
     }
+
     public Date getDataEmp() {
         return dataEmp;
     }
+
     public void setDataEmp(Date dataEmp) {
         this.dataEmp = dataEmp;
     }
+
     public Date getDataDevol() {
         return dataDevol;
     }
+
     public void setDataDevol(Date dataDevol) {
         this.dataDevol = dataDevol;
     }
+
     public boolean isDevolvido() {
         return devol;
     }
+
     public void setDevolvido(boolean devol) {
         this.isDevolvido = devol;
     }
