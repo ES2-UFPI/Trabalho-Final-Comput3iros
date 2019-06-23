e258c3c2fd35ae821209a273b5535f283995ec3c
Hilderlan <hilakt9698@gmail.com>
2019-06-23 19:25:01 -0300

    Modificado o metodo de acesso aos menus

--- src/SubMenuCadastro.java
+++ src/SubMenuCadastro.java
@@ -2,40 +2,45 @@ import java.util.Scanner;
 
 public class SubMenuCadastro {
     public static void menuCadastro(Locadora l) {
-        System.out.println("\n       ### Locadora - Sistema de Empréstimos e Devoluções ###");
-        System.out.println("\n                  ===================================");
-        System.out.println("                  |             Cadastro            |");
-        System.out.println("                  ===================================");
-        System.out.println("                  |                                 |");
-        System.out.println("                  |     1 - Locatário               |");
-        System.out.println("                  |     2 - Exemplar                |");
-        System.out.println("                  |     3 - Configuração            |");
-        System.out.println("                  |     0 - Sair                    |");
-        System.out.println("                  ===================================\n");
+        int opcao;
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("                  Opção ->  ");
-        int opcao = scanner.nextInt();
+        do {
+            Scanner scanner = new Scanner(System.in);
 
-        while (opcao < 0 || opcao > 3) {
+            System.out.println("\n       ### Locadora - Sistema de Empréstimos e Devoluções ###");
+            System.out.println("\n                  ===================================");
+            System.out.println("                  |             Cadastro            |");
+            System.out.println("                  ===================================");
+            System.out.println("                  |                                 |");
+            System.out.println("                  |     1 - Locatário               |");
+            System.out.println("                  |     2 - Exemplar                |");
+            System.out.println("                  |     3 - Configuração            |");
+            System.out.println("                  |     0 - Sair                    |");
+            System.out.println("                  ===================================\n");
+
             System.out.print("                  Opção ->  ");
             opcao = scanner.nextInt();
-            scanner.close();
-        }
 
-        switch (opcao) {
-        case 1: // Cadastro de Locatario
-            break;
-        case 2: // Cadastro de Exemplar
-            SubMenuExemplar.menuExemplar(l);
-            break;
-        case 3: // Cadastro de Configuracao
-            break;
-        case 0: // Sair
-            break;
-        default:
-            System.out.println("Opção Inválida!");
-            break;
-        }
+            while (opcao < 0 || opcao > 3) {
+                System.out.print("                  Opção ->  ");
+                opcao = scanner.nextInt();
+            }
+
+            switch (opcao) {
+            case 1: // Cadastro de Locatario
+                break;
+            case 2: // Cadastro de Exemplar
+                SubMenuExemplar.menuExemplar(l);
+                break;
+            case 3: // Cadastro de Configuracao
+                break;
+            case 0: // Sair
+                // scanner.close();
+                break;
+            default:
+                System.out.println("Opção Inválida!");
+                break;
+            }
+        } while (opcao != 0);
     }
 }
--- src/SubMenuExemplar.java
+++ src/SubMenuExemplar.java
@@ -2,69 +2,71 @@ import java.util.Scanner;
 
 public class SubMenuExemplar {
     public static void menuExemplar(Locadora l) {
-        Scanner in = new Scanner(System.in);
+        int opcao;
+        do {
+            Scanner in = new Scanner(System.in);
 
-        System.out.println("\n       ### Locadora - Sistema de Empréstimos e Devoluções ###");
-        System.out.println("\n                  ===================================");
-        System.out.println("                  |             Exemplar            |");
-        System.out.println("                  ===================================");
-        System.out.println("                  |                                 |");
-        System.out.println("                  |     1 - Livro                   |");
-        System.out.println("                  |     2 - Artigo                  |");
-        System.out.println("                  |                                 |");
-        System.out.println("                  |     0 - Sair                    |");
-        System.out.println("                  ===================================\n");
+            System.out.println("\n       ### Locadora - Sistema de Empréstimos e Devoluções ###");
+            System.out.println("\n                  ===================================");
+            System.out.println("                  |             Exemplar            |");
+            System.out.println("                  ===================================");
+            System.out.println("                  |                                 |");
+            System.out.println("                  |     1 - Livro                   |");
+            System.out.println("                  |     2 - Artigo                  |");
+            System.out.println("                  |                                 |");
+            System.out.println("                  |     0 - Sair                    |");
+            System.out.println("                  ===================================\n");
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("                  Opção ->  ");
-        int opcao = scanner.nextInt();
-
-        while (opcao < 0 || opcao > 2) {
+            Scanner scanner = new Scanner(System.in);
             System.out.print("                  Opção ->  ");
             opcao = scanner.nextInt();
-            scanner.close();
-        }
 
-        switch (opcao) {
-        case 1: // Cadastro de Livro
-            System.out.println("    -> Codigo: ");
-            String codigo = in.nextLine();
-            System.out.println("    -> Titulo: ");
-            String titulo = in.nextLine();
-            System.out.println("    -> Autor: ");
-            String autor = in.nextLine();
-            System.out.println("    -> Quantidade: ");
-            int quantidade = in.nextInt();
-            System.out.println("    -> Volume: ");
-            String volume = in.nextLine();
-            System.out.println("    -> Quantidade de paginas: ");
-            int numPaginas = in.nextInt();
+            while (opcao < 0 || opcao > 2) {
+                System.out.print("                  Opção ->  ");
+                opcao = scanner.nextInt();
+                scanner.close();
+            }
 
-            l.cadastrarExemplar(codigo, titulo, autor, quantidade, volume, numPaginas);
+            switch (opcao) {
+            case 1: // Cadastro de Livro
+                System.out.println("    -> Codigo: ");
+                String codigo = in.nextLine();
+                System.out.println("    -> Titulo: ");
+                String titulo = in.nextLine();
+                System.out.println("    -> Autor: ");
+                String autor = in.nextLine();
+                System.out.println("    -> Volume: ");
+                String volume = in.nextLine();
+                System.out.println("    -> Quantidade: ");
+                int quantidade = in.nextInt();
+                System.out.println("    -> Quantidade de paginas: ");
+                int numPaginas = in.nextInt();
 
-            in.close();
-            break;
-        case 2: // Cadastro de Artigo
-            System.out.println("    -> Codigo: ");
-            String codigo = in.nextLine();
-            System.out.println("    -> Titulo: ");
-            String titulo = in.nextLine();
-            System.out.println("    -> Autor: ");
-            String autor = in.nextLine();
-            System.out.println("    -> Quantidade: ");
-            int quantidade = in.nextInt();
-            System.out.println("    -> Revista: ");
-            String revista = in.nextLine();
+                l.cadastrarExemplar(codigo, titulo, autor, quantidade, volume, numPaginas);
 
-            l.cadastrarExemplar(codigo, titulo, autor, quantidade, revista);
+                break;
+            case 2: // Cadastro de Artigo
+                System.out.println("    -> Codigo: ");
+                codigo = in.nextLine();
+                System.out.println("    -> Titulo: ");
+                titulo = in.nextLine();
+                System.out.println("    -> Autor: ");
+                autor = in.nextLine();
+                System.out.println("    -> Revista: ");
+                String revista = in.nextLine();
+                System.out.println("    -> Quantidade: ");
+                quantidade = in.nextInt();
 
-            in.close();
-            break;
-        case 0: // Sair
-            break;
-        default:
-            System.out.println("Opção Inválida!");
-            break;
-        }
+                l.cadastrarExemplar(codigo, titulo, autor, quantidade, revista);
+
+                break;
+            case 0: // Sair
+                in.close();
+                break;
+            default:
+                System.out.println("Opção Inválida!");
+                break;
+            }
+        } while (opcao != 0);
     }
 }
